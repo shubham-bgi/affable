b51a9263dee26f6e00ff3197d2310f2827c83657
Shubham <shubhamaggarwal3011@gmail.com>
2023-12-18 01:49:52 +0530

    updated max min dto

--- src/modules/follower/dto/task.dto.ts
+++ src/modules/follower/dto/task.dto.ts
@@ -1,18 +1,24 @@
 import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNumber, IsOptional } from "class-validator";
+import { IsNumber, IsOptional, Min, Max } from "class-validator";
 
 export class EnQueueDto {
   @ApiPropertyOptional({ default: 1_000_000 })
   @IsNumber()
+  @Min(1_000_000)
+  @Max(2_000_000)
   @IsOptional()
   start: number;
 
   @ApiPropertyOptional({ default: 2_000_000 })
   @IsNumber()
+  @Min(1_000_000)
+  @Max(2_000_000)
   @IsOptional()
   end: number;
 
   @ApiPropertyOptional({ default: 100 })
+  @Min(5)
+  @Max(1000)
   @IsNumber()
   @IsOptional()
   batch: number;
